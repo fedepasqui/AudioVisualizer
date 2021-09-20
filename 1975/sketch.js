7fa7888089447fb63c1e890874a1f2f8e276edad
Federica Pasquini <90723641+fedepasqui@users.noreply.github.com>
2021-09-20 13:04:29 +0200

    Add files via upload

---
+++ 1975/sketch.js
@@ -0,0 +1,124 @@
+var song;
+var img;
+var fft; //fast forward transform
+var particles = [];
+
+//To insure that the audio file is loaded properly before anything else happens
+function preload() {
+  song = loadSound("SomebodyElse.mp3");
+  img = loadImage("albumcover2.jpg");
+}
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  angleMode(DEGREES);
+  imageMode(CENTER);
+  rectMode(CENTER);
+  fft = new p5.FFT(0.3);
+
+  img.filter(BLUR, 3);
+}
+
+function draw() {
+  background(15);
+
+  translate(width / 2, height / 2);
+
+  fft.analyze();
+  amp = fft.getEnergy(20, 200);
+
+  push();
+  if (amp > 230) {
+    rotate(random(-0.5, 0.5));
+  }
+
+  image(img, 0, 0, width + 100, height + 100);
+  pop();
+
+  var alpha = map(amp, 0, 255, 180, 150);
+  fill(0, alpha);
+  noStroke();
+  rect(0, 0, width, height);
+
+  stroke(255);
+  strokeWeight(3);
+  noFill();
+
+  var wave = fft.waveform();
+
+  for (var t = -1; t <= 1; t += 2) {
+    beginShape();
+    for (var i = 0; i <= 180; i += 0.5) {
+      var index = floor(map(i, 0, 180, 0, wave.length - 1));
+
+      var r = map(wave[index], -1, 1, 150, 350);
+
+      var x = r * sin(i) * t;
+      var y = r * cos(i);
+      vertex(x, y);
+    }
+    endShape();
+  }
+
+  var p = new Particle();
+  particles.push(p);
+
+  for (var i = 0; i < particles.length; i++) {
+    if (!particles[i].edges()) {
+      particles[i].update(amp > 170);
+      particles[i].show();
+    } else {
+      particles.splice(i, 1);
+    }
+  }
+}
+
+//To ensure that the audio plays regardless of the browser
+function mouseClicked() {
+  if (song.isPlaying()) {
+    song.pause();
+  } else {
+    song.play();
+  }
+}
+
+class Particle {
+  constructor() {
+    this.pos = p5.Vector.random2D().mult(250);
+    this.vel = createVector(0, 0);
+    this.acc = this.pos.copy().mult(random(0.0001, 0.00001));
+
+    this.w = random(3, 5);
+
+    this.color = [random(0, 255), random(0, 20), random(0, 200)];
+  }
+
+  update(cond) {
+    this.vel.add(this.acc);
+    this.pos.add(this.vel);
+    if (cond) {
+      this.pos.add(this.vel);
+      this.pos.add(this.vel);
+      this.pos.add(this.vel);
+    }
+  }
+
+  edges() {
+    if (
+      this.pos.x < -width / 2 ||
+      this.pos.x > width / 2 ||
+      this.pos.y < -height / 2 ||
+      this.pos.y > height / 2
+    ) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  show() {
+    noStroke();
+    fill(this.color);
+    ellipse(this.pos.x, this.pos.y, 4);
+  }
+}
